881c8228e7a6a140b39d3681df93cdbc82381861
Patrick Davey <patrick.davey@gmail.com>
2016-10-26 21:08:20 +0200

    Add test to cover index route switching.

--- test/controllers/cast_controller_test.exs
+++ test/controllers/cast_controller_test.exs
@@ -1,9 +1,22 @@
 defmodule Caster.PageControllerTest do
   use Caster.ConnCase
+  alias Caster.CustomCast
+  alias Caster.VimCast
 
   test "GET /", %{conn: conn} do
+    Repo.insert!(%VimCast{title: "vimcasttest", url: "a"})
+    Repo.insert!(%CustomCast{title: "customcasttest", url: "a" })
     conn = get conn, "/"
-    assert html_response(conn, 200)
+    assert html_response(conn, 200) =~ "customcasttest"
+    refute html_response(conn, 200) =~ "vimcasttest"
+  end
+
+  test "GET /?type=vimcast", %{conn: conn} do
+    Repo.insert!(%VimCast{title: "vimcasttest", url: "a"})
+    Repo.insert!(%CustomCast{title: "customcasttest", url: "a" })
+    conn = get conn, "/?type=vimcast"
+    refute html_response(conn, 200) =~ "customcasttest"
+    assert html_response(conn, 200) =~ "vimcasttest"
   end
 
   test "renders page not found when id is nonexistent", %{conn: conn} do
