1c64263973ebdd16ebb2e6af860f3ad2d9ddc000
Patrick Davey <patrick.davey@gmail.com>
2016-12-17 17:11:09 +0100

    Use scss for the stylesheets.
    I haven't pulled in Bootstrap to compile it yet,
    as it doesn't seem incredibly straightforward
    unfortunately. I mean there is a way, but I
    can't be bothered doing it at the moment.
    
    If you're reading this, PR's welcome ;)

--- config/dev.exs
+++ config/dev.exs
@@ -18,7 +18,7 @@ config :caster, Caster.Endpoint,
 config :caster, Caster.Endpoint,
   live_reload: [
     patterns: [
-      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
+      ~r{priv/static/.*(js|css|scss|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
       ~r{web/templates/.*(eex)$}
