e68dbe9f93d5d42d672b9f0f7d9da6cb840def1a
Patrick Davey <patrick.davey@gmail.com>
2017-01-29 15:20:54 +1300

    Fix for youtube-dl sometimes altering filename
    Sometimes youtube-dl will alter a filename when it
    attempts to download it and it turns out to require
    a different format.
    
    It wasn't super straightforward to me how to get back
    the rewritten filename in a standard way. So, I cheat.
    
    I now prepend the unique-id to the start of the filename,
    and then, even if the extension is altered as a part of the
    downloading I can find it.
    
    So, after the file has been downloaded, I do a wildcard
    match on all the filenames starting with the specific ID
    and then I assume that there's only one match and that's the
    one I want.
    
    I will alter this code to raise an exception if there's more
    than one. I think.

--- web/models/custom_cast_downloader.ex
+++ web/models/custom_cast_downloader.ex
@@ -33,8 +33,16 @@ defmodule Caster.CustomCastDownloader do
       end
     end
 
+    def custom_id(%{url: url}) do
+      {custom_id, 0} = System.cmd("youtube-dl", ["--get-id", url])
+      custom_id
+      |> String.trim
+    end
+
     def get_filepath(%Cast{url: url}) do
       {filepath, 0} = System.cmd("youtube-dl", ["--restrict-filenames", "--get-filename", url])
+      filepath = "#{custom_id(%{url: url})}_#{filepath}"
+      # we are going to append our custom_id to the start of the filename
       filepath = Path.expand("#{Application.get_env(:caster, :root_downloads_directory)}/#{@download_subdirectory}/#{String.trim_trailing(filepath)}", Application.app_dir(:caster, "priv"))
       {:ok, filepath}
     end
@@ -44,6 +52,15 @@ defmodule Caster.CustomCastDownloader do
         {:ok, filepath} = get_filepath(cast)
         Mix.Generator.create_directory(Path.dirname(filepath))
         System.cmd("youtube-dl", ["-o", filepath,  url])
+        # now the filepath may have been updated, so get the ID again.
+        id = custom_id(%{url: url})
+        filepath = String.split(filepath, id)
+                   |> List.first
+                   |> Kernel.<>(id)
+                   |> Kernel.<>("*")
+                   |> Path.wildcard
+                   |> List.first
+
         changeset = CustomCast.changeset(cast, %{file_location: filepath})
         Repo.update!(changeset)
         # the broadcasting _really_ shouldn't be here.
